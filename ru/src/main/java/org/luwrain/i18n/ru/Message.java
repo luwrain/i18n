0fe7c80a9c0fd226a717bc35effb4be100b7bbed
Michael Pozhidaev <michael.pozhidaev@gmail.com>
2015-08-06 05:38:26 +0600

    Forgotten files from the previous commit

---
+++ ru/src/main/java/org/luwrain/i18n/ru/Message.java
@@ -0,0 +1,45 @@
+/*
+   Copyright 2012-2015 Michael Pozhidaev <michael.pozhidaev@gmail.com>
+
+   This file is part of the Luwrain.
+
+   Luwrain is free software; you can redistribute it and/or
+   modify it under the terms of the GNU General Public
+   License as published by the Free Software Foundation; either
+   version 3 of the License, or (at your option) any later version.
+
+   Luwrain is distributed in the hope that it will be useful,
+   but WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+   General Public License for more details.
+*/
+
+package org.luwrain.i18n.ru;
+
+public class Message implements org.luwrain.app.message.Strings
+{
+    @Override public String appName()
+    {
+	return "Сообщение";
+    }
+
+    @Override public String noMailStoring()
+    {
+	return "Нет подключения для хранения электронной почты";
+    }
+
+    @Override public String withoutSubject()
+    {
+	return "ННет темы сообщения";
+    }
+
+    @Override public String emptyRecipient()
+    {
+	return "Пустой получатель";
+    }
+
+    @Override public String errorSendingMessage()
+    {
+	return "Произошла ошибка при отправлении сообщения";
+    }
+}
