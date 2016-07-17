7cbbc055b3c3b0441d56e7d4134e24f1b1d146d7
Michael Pozhidaev <michael.pozhidaev@gmail.com>
2016-07-18 06:18:43 +0700

    Minor fixes to player translation

--- ru/src/main/java/org/luwrain/i18n/ru/Extension.java
+++ ru/src/main/java/org/luwrain/i18n/ru/Extension.java
@@ -53,7 +53,7 @@ public class Extension extends org.luwrain.core.extensions.EmptyExtension
 	save("opds", "Каталоги книг", new AppOpds(), ext);
 	saveWithProxy("narrator", "Рассказчик", org.luwrain.app.narrator.Strings.class, ext);
 	saveWithProxy("wifi", "Подключение к WiFi-сетям", org.luwrain.app.wifi.Strings.class, ext);
-	//	saveWithProxy("player", "Медиаплеер", org.luwrain.app.player.Strings.class, ext);
+	saveWithProxy("player", "Медиаплеер", org.luwrain.app.player.Strings.class, ext);
 	save("calc", "Калькулятор", new AppCalc(), ext);
  	ext.addCommandTitle("ru", "reader-search-google", "Поиск в Google");
  	ext.addCommandTitle("ru", "reader-luwrain-homepage", "Домашняя страница LUWRAIN");
